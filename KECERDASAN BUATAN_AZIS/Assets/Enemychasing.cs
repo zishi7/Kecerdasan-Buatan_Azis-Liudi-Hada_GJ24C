9e41c28e582fc145ef980ffe4eff3efd382d18ff
zishi7 <azizliudihada1988@gmail.com>
2026-03-05 12:50:34 +0700

    Kecerdasan buatan

---
+++ KECERDASAN BUATAN_AZIS/Assets/Enemychasing.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemychasing : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent agent;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
